de3ca07a2866a5cd126cfaea5eb293cf05b59988
Wise Duho <duhowise@gmail.com>
2021-12-10 07:32:23 +0000

    Add project files.

---
+++ Src/PackIT.Domain/Entities/PackingList.cs
@@ -0,0 +1,9 @@
+﻿namespace PackIT.Domain
+{
+    public class PackingList
+    {
+        public Guid Id { get; private set; }
+        public string Name { get;private set; }
+        public string Localisation { get;private set; }
+    }
+}
